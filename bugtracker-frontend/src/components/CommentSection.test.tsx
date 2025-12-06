613dd6800d9fdee325676dc39acfda7902ad89e2
Sorav Dangi <sorav82371@gmail.com>
2025-12-06 10:45:14 +0530

    $perparing to depoy 13

--- bugtracker-frontend/src/components/CommentSection.test.tsx
+++ bugtracker-frontend/src/components/CommentSection.test.tsx
@@ -52,7 +52,7 @@ describe("CommentSection", () => {
 
     await waitFor(() => {
       expect(fetch).toHaveBeenCalledWith(
-        "http://localhost:8080/api/bugs/1/comments",
+        "http://localhost:10000/api/bugs/1/comments",
         {
           method: "POST",
           headers: {
